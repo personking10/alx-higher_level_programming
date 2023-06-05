860a0160155f2bf6cbd4e091a46c0587c1bd655c
darsh2 <tefakong33@gmail.com>
2023-06-05 15:59:05 -0700

    a

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,17 +1,19 @@
-#ifndef MOSTAFA_H
-#define MOSTAFA_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
-/**
- * struct listint_s single linked list
- * @i: int
- * @next: next node
- * @Description: single node struct
- */
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
 typedef struct listint_s
 {
-	int i;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
@@ -19,4 +21,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-#endif
+
+#endif /* LISTS_H */
